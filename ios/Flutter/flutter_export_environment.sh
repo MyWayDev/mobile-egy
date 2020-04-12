024548e62ba2719ca6daba88c709e262983f0565
Shady Abouauf <shadyabouauf@Shadys-MacBook-Pro.local>
2020-04-12 08:31:25 +0200

    abdo- 2020-04-12 attempt to run iOS device

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/shadyabouauf/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/shadyabouauf/proj/mobile_egy"
-export "FLUTTER_TARGET=/Users/shadyabouauf/proj/mobile_egy/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/shadyabouauf/Documents/mobile_egy"
+export "FLUTTER_TARGET=/Users/shadyabouauf/Documents/mobile_egy/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/shadyabouauf/flutter/bin/cache/artifacts/engine/ios"
