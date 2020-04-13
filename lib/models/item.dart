0071fd4621a4383105ced72727260c71bc32070d
Shady Abouauf <shadyabouauf@Shadys-MacBook-Pro.local>
2020-04-13 12:10:00 +0200

    Abdo - item.dart change item image url to take empty array default

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -68,7 +68,7 @@ class Item {
         price = snapshot.value['price'] ?? 0,
         bp = snapshot.value['bp'] ?? 0,
         bv = snapshot.value['bv'] ?? 0.0,
-        image = snapshot.value['image'] ?? "",
+        image = snapshot.value['image'] ?? [],
         imageUrl = snapshot.value['imageUrl'] ?? "",
         size = snapshot.value['size'] ?? "",
         unit = snapshot.value['unit'] ?? "",
